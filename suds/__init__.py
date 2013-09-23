c6bbf36f218111173af3c726d25c8bb55aaa5bea
Ilya Baryshev <baryshev@gmail.com>
2013-09-23 14:17:06 +0400

    Fixed WebFault with unicode string
    Merged patch from: https://bitbucket.org/apkawa/suds/commits/3754d944a54faa68a086c05f2916478a7c1d1f39

--- suds/__init__.py
+++ suds/__init__.py
@@ -70,11 +70,38 @@ class SoapHeadersNotPermitted(Exception):
         """
     def __init__(self, name):
         Exception.__init__(self, self.msg % name)
-    
+
+
+def smart_str(s, encoding='utf-8', errors='strict'):
+    """
+    Returns a bytestring version of 's', encoded as specified in 'encoding'.
+
+    If strings_only is True, don't convert (some) non-string-like objects.
+
+    from django
+    """
+    if not isinstance(s, basestring):
+        try:
+            return str(s)
+        except UnicodeEncodeError:
+            if isinstance(s, Exception):
+                # An Exception subclass containing non-ASCII data that doesn't
+                # know how to print itself properly. We shouldn't raise a
+                # further exception.
+                return ' '.join([smart_str(arg, encoding, errors) for arg in s])
+            return unicode(s).encode(encoding, errors)
+    elif isinstance(s, unicode):
+        return s.encode(encoding, errors)
+    elif s and encoding != 'utf-8':
+        return s.decode('utf-8', errors).encode(encoding, errors)
+    else:
+        return s
+
+
 class WebFault(Exception):
     def __init__(self, fault, document):
         if hasattr(fault, 'faultstring'):
-            Exception.__init__(self, "Server raised fault: '%s'" % fault.faultstring)
+            Exception.__init__(self, smart_str("Server raised fault: '%s'" % fault.faultstring))
         self.fault = fault
         self.document = document
 
